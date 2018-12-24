ea184cd640571ca47c204d5186e9bccbabf452f0
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-24 16:17:22 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,4 +1,4 @@
-# passengers = {
+passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
 # suite_c: "Alfred Tennyson", 
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
 end
